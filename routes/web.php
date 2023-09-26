9adcd4c4e52fa8f9e46fa74bcc4a749898e05174
Amdouni-Dev <amdouni.mouna@esprit.tn>
2023-09-26 01:22:00 +0100

    Crud Event

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,27 @@ Route::get('/test2',[\App\Http\Controllers\TestController::class,'test2']);
 Route::get('/testForm',[\App\Http\Controllers\TestController::class,'testForm']);
 
 Route::get('/testTemplate',[\App\Http\Controllers\TestController::class,'testTemplate']);
+
+// Partie Event
+Route::get('/events',[\App\Http\Controllers\EventController::class,'affiche']);
+Route::post('/events', [EventController::class, 'store'])->name('events.store');
+
+Route::get('/createEvent', [EventController::class, 'create']);
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->name('events.destroy');
+//Route::get('/events/{id}', [EventController::class, 'update'])->name('events.update');
+Route::get('/events/{id}', [EventController::class, 'edit'])->name('events.edit');
+Route::put('/events/{id}', [EventController::class, 'update'])->name('events.update');
+
+
+Route::get('/eventsShow/{id}', [EventController::class, 'show'])->name('events.show');
+
+
+//Route::put('/events', [EventController::class, 'update']);
+
+
+
+
+
+
+
 
